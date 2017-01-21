b3c4860bb042963438ff05246b7cc1ba40080bef
Andrew Burnett <andrew@andburn.info>
2017-01-21 13:01:04 +0000

    Compact: clean hearthsim mirror messages

--- addons/compact.py
+++ addons/compact.py
@@ -9,7 +9,7 @@ import datetime
 import hexchat
 
 
-NICK_LEN = 10
+NICK_LEN = 12
 prev_dates = {}
 
 
@@ -25,25 +25,48 @@ def set_previous(channel, date):
 	prev_dates[channel] = date
 
 
+def truncate(str, length=NICK_LEN):
+	if len(str) > length:
+		return str[:length - 1] + "~"
+	return str
+
+
 def print_date_if_new(channel, time):
 	server_time = datetime.datetime.fromtimestamp(time)
 	prev_day = get_previous(channel)
 	if server_time.date() > prev_day.date():
 		set_previous(channel, server_time)
-		print(server_time.strftime("\n\002%A, %d %b %Y"))
+		print(server_time.strftime("\n\002%A, %d %b %Y\n"))
 
 
 def handle_message(word, word_eol, userdata, attributes):
-	if attributes.time:
-		channel = hexchat.get_info("channel")
-		print_date_if_new(channel, attributes.time)
+	if not attributes.time:
+		return hexchat.EAT_NONE
 
-	return hexchat.EAT_NONE
+	channel = hexchat.get_info("channel")
+	print_date_if_new(channel, attributes.time)
+
+	# check for hearthsim mirror bot messages
+	match = re.search(r':\[(discord|gitter|irc)\]\s+<(.*?)>\s+(.*)', word_eol[3])
+	if match:
+		# rearrange the message
+		nick = truncate(match.group(2), 10) + "|" + match.group(1)[:1].upper()
+		message = match.group(3)
+	else:
+		# otherwise chop nick and use message as given
+		message = word_eol[3][1:]
+		match = re.search(r':(.+?)!', word[0])
+		if match:
+			nick = truncate(match.group(1))
+		else:
+			nick = word[0][:NICK_LEN]
+
+	hexchat.emit_print("Channel Message", nick, message, time=attributes.time)
+	return hexchat.EAT_ALL
 
 
 def handle_notice(word, word_eol, userdata, attributes):
 	pass
-
 
 hexchat.hook_server_attrs("PRIVMSG", handle_message)
 hexchat.hook_server_attrs("NOTICE", handle_notice)
